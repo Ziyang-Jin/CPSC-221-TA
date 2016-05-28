243e89e7ac00f5ba18cf22c07564cc83cfd2fa4e
Ziyang Jin <f4a0b@ugrad.cs.ubc.ca>
2016-05-27 22:01:19 -0700

    Update linkedlist.cpp

--- assignment2/linkedlist.cpp
+++ assignment2/linkedlist.cpp
@@ -2,8 +2,6 @@
 #include <stdexcept>
 using namespace std;
 
-// helper function for deep copy
-// Used by copy constructor and operator=
 template <typename T>
 void LinkedList<T>::CopyList(const LinkedList& ll) {
     DeleteList();
@@ -22,8 +20,6 @@ void LinkedList<T>::CopyList(const LinkedList& ll) {
     }
 }
 
-// helper function for deep delete
-// Used by destructor and copy/assignment
 template <typename T>
 void LinkedList<T>::DeleteList() {
     Node<T> *curr = front, *temp;
@@ -36,27 +32,15 @@ void LinkedList<T>::DeleteList() {
     front = back = NULL;
 }
 
-// default constructor
 template <typename T>
 LinkedList<T>::LinkedList() : size(0), front(NULL), back(NULL) {}
 
-// copy constructor, performs deep copy of list elements
 template <typename T>
-LinkedList<T>::LinkedList(const LinkedList& ll) : size(0), front(NULL), back(NULL) {
-    CopyList(ll);
-}
+LinkedList<T>::LinkedList(const LinkedList& ll) : size(0), front(NULL), back(NULL) { CopyList(ll); }
 
-// destructor
 template <typename T>
-LinkedList<T>::~LinkedList() {
-    DeleteList();
-}
+LinkedList<T>::~LinkedList() { DeleteList(); }
 
-// MUTATORS
-
-// Inserts an item at the front of the list
-// POST: List contains item at position 0
-// PARAM: item = item to be inserted
 template <typename T>
 void LinkedList<T>::InsertFront(T item) {
     Node<T> *temp = new Node<T>(item);
@@ -69,9 +53,6 @@ void LinkedList<T>::InsertFront(T item) {
     }
 }
 
-// Inserts an item at the back of the list
-// POST: List contains item at back
-// PARAM: item = item to be inserted
 template <typename T>
 void LinkedList<T>::InsertBack(T item) {
     Node<T> *temp = new Node<T>(item);
@@ -84,11 +65,6 @@ void LinkedList<T>::InsertBack(T item) {
     }
 }
 
-// Inserts an item in position p (0-indexed)
-// Throws exception for invalid index
-// PRE: 0 <= p <= size
-// POST: List contains item at position p
-// PARAM: item = item to be inserted, p = position where item will be inserted
 template <typename T>
 void LinkedList<T>::InsertAt(T item, int p) {
     if(p < 0 || p > size) // invalid index
@@ -111,8 +87,7 @@ void LinkedList<T>::InsertAt(T item, int p) {
         back = temp;
         return;
     }
-    // insertMiddle
-    Node<T> *mark = front;
+    Node<T> *mark = front; // insertMiddle
     while(p-- > 0)
         mark = mark->next;
     temp->prev = mark->prev;
@@ -121,8 +96,6 @@ void LinkedList<T>::InsertAt(T item, int p) {
     mark->prev = temp;
 }
 
-// Removes and returns an item from position p (0-indexed)
-// Throws exception if list is empty or index invalid
 template <typename T>
 T LinkedList<T>::RemoveAt(int p) {
     if(p < 0 || p >= size) // invalid index 
@@ -146,9 +119,6 @@ T LinkedList<T>::RemoveAt(int p) {
     return d;
 }
 
-// Appends the contents of the parameter list to the back of this list as a deep copy
-// PRE:
-// POST: List contains every element of ll appended to back, size is updated appropriately
 template <typename T>
 void LinkedList<T>::Append(const LinkedList& ll) {
     Node<T> *temp;
@@ -162,13 +132,6 @@ void LinkedList<T>::Append(const LinkedList& ll) {
     size += append_size;
 }
 
-// Removes duplicates from the list, preserving existing order of remaining items.
-// The LAST occurrence of any duplicate (relative to the front of the list)
-// is the one which remains.
-// Efficiency will not be a priority as long as the behaviour above is satisfied.
-// PRE:
-// POST: List contains no duplicates, front and back point to the appropriate nodes
-// PARAM:
 template <typename T>
 void LinkedList<T>::RemoveDuplicates() {
     Node<T> *temp = front, *check, *dele;
@@ -199,21 +162,16 @@ void LinkedList<T>::RemoveDuplicates() {
     }
 }
 
-// ACCESSORS
-
-// Return size of list
 template <typename T>
 int LinkedList<T>::Size() const {
     return size;
 }
 
-// Returns whether the list is empty
 template <typename T>
 bool LinkedList<T>::IsEmpty() const {
     return !size;
 }
 
-// Returns existence of item
 template <typename T>
 bool LinkedList<T>::Contains(T item) const {
     Node<T> *curr = front;
@@ -225,8 +183,6 @@ bool LinkedList<T>::Contains(T item) const {
     return false;
 }
 
-// Returns item at index (0-indexed)
-// Throws exception for invalid index
 template <typename T>
 T LinkedList<T>::ElementAt(int p) const {
     if(p < 0 || p > (size-1))
@@ -237,12 +193,6 @@ T LinkedList<T>::ElementAt(int p) const {
     return curr->data;
 }
 
-// OVERLOADED OPERATORS
-
-// overloaded assignment operator
-// must work in the following cases:
-// list2 = list1 -> general case
-// list2 = list2 -> should do nothing
 template <typename T>
 LinkedList<T>& LinkedList<T>::operator=(const LinkedList<T>& ll) {
     if(this != &ll)
